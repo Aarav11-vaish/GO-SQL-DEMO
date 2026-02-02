fcd2d77d1b80e9a5365ddab29bf7c5bcdaadf417
Aarav Vaish <aaravvaish2004@gmail.com>
2026-02-02 16:17:16 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	// "database/sql"
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	// "log"
+	// "os"
+)
+
+type Record struct {
+	InternID int
+	Name     string
+	Stipend  string
+}
+
+func (r Record) String() string {
+	return fmt.Sprintf("InternID: %d, Name: %s, Stipend: %s", r.InternID, r.Name, r.Stipend)
+}
+
+func getRecordsWithID(db *sql.DB, id int) ([]Record, error) {
+	rows, err := db.Query("select intern_id, name, stipend from intern_info where intern_id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var records []Record
+	for rows.Next() {
+		var record Record
+		if err := rows.Scan(&record.InternID, &record.Name, &record.Stipend); err != nil {
+			return nil, err
+		}
+		records = append(records, record)
+
+	}
+	return records, nil
+}
+func main() {
+	// Capture connection properties.
+	configure := mysql.NewConfig()
+	fmt.Println("configure has the following data ", configure)
+	configure.User = os.Getenv("DBUSER")
+	configure.Passwd = os.Getenv("DBPASS")
+	configure.Net = "tcp"
+	configure.Addr = "127.0.0.1:3306"
+	configure.DBName = "recordings"
+
+	db, err := sql.Open("mysql", configure.FormatDSN())
+	if err != nil {
+		fmt.Println("Error connecting to database: ", err)
+		return
+	}
+	defer db.Close()
+	var records []Record
+	id := 213
+	fmt.Scan(&id)
+	records, err = getRecordsWithID(db, id)
+	if err != nil {
+		fmt.Println("Error fetching records: ", err)
+		return
+	}
+	for _, record := range records {
+		fmt.Printf("%v\n", record)
+	}
+	// how to call 20th line?
+
+}
